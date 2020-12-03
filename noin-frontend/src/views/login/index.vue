754678681239f9225eb36b8ea6293fd698b18365
jinhgg <970402782@qq.com>
2020-12-03 16:06:57 +0800

    删除vue无效文件，创建login组件配好路由（可用作模板）

---
+++ noin-frontend/src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+   <h1>登录</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  components: {},
+  data () {
+    return {}
+  },
+  created () {},
+  methods () {}
+}
+</script>
