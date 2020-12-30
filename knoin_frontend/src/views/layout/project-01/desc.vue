2dee74ace9b266ca812d56c0e6986616a017b6f3
jinhgg <970402782@qq.com>
2020-12-30 10:07:37 +0800

    备份提交

--- knoin_frontend/src/views/layout/project-01/desc.vue
+++ knoin_frontend/src/views/layout/project-01/desc.vue
@@ -5,32 +5,97 @@
       <b-tabs card>
         <b-tab title="新建项目" active>
           <b-card-body>
-            <b-card
-              class="basicInfo"
-              header="基本信息"
-              header-tag="header"
-              header-bg-variant=""
-            >
+            <div class="basicInfo">
+              <div hidden="hidden">
+                <b-button id="selectProj" v-b-modal.modal-1
+                  >Launch demo modal</b-button
+                >
+                <b-modal
+                  ref="proj-modal"
+                  size="xl"
+                  id="modal-1"
+                  title="样本选择"
+                  hide-footer
+                >
+                  <b-table
+                    selectable
+                    ref="selectableTable"
+                    :items="projectList"
+                    :fields="project_fields"
+                    select-mode="single"
+                    @row-selected="onRowSelected"
+                  >
+                    <template #cell(选择)="{ rowSelected }">
+                      <template v-if="rowSelected">
+                        <span aria-hidden="true">&check;</span>
+                        <span class="sr-only">Selected</span>
+                      </template>
+                      <template v-else>
+                        <span aria-hidden="true">&nbsp;</span>
+                        <span class="sr-only">Not selected</span>
+                      </template>
+                    </template>
+                  </b-table>
+                  <b-button class="mt-3" block @click="projOnSelect"
+                    >确定</b-button
+                  >
+                </b-modal>
+              </div>
+              <h4 style="margin-bottom: 30px">基本信息</h4>
+              <b-form-group
+                label="文件导入:"
+                label-for="fileBasic"
+                label-cols-sm="3"
+              >
+                <b-form-file
+                  id="fileBasic"
+                  size="sm"
+                  accept=".xlsx"
+                  @change="readExcel($event)"
+                  ref="file-proj"
+                ></b-form-file>
+                <b-button style="float:right;width:62px"  size="sm" @click="clearFiles">重置</b-button>
+              </b-form-group>
+
               <b-form-group label-cols-sm="3" label="姓名:" label-for="name">
-                <b-form-input id="name" size="sm"></b-form-input>
+                <b-form-input
+                  v-model="this.basicForm.name"
+                  id="name"
+                  size="sm"
+                ></b-form-input>
               </b-form-group>
 
               <b-form-group label-cols-sm="3" label="年龄:" label-for="age">
-                <b-form-input id="age" size="sm"></b-form-input>
+                <b-form-input
+                  v-model="this.basicForm.age"
+                  id="age"
+                  size="sm"
+                ></b-form-input>
               </b-form-group>
               <b-form-group label-cols-sm="3" label="性别:" label-for="gender">
-                <b-form-input id="gender" size="sm"></b-form-input>
+                <b-form-input
+                  v-model="this.basicForm.gender"
+                  id="gender"
+                  size="sm"
+                ></b-form-input>
               </b-form-group>
               <b-form-group label-cols-sm="3" label="编号:" label-for="no">
-                <b-form-input id="no" size="sm"></b-form-input>
+                <b-form-input
+                  v-model="this.basicForm.no"
+                  id="no"
+                  size="sm"
+                ></b-form-input>
               </b-form-group>
-
               <b-form-group
                 label-cols-sm="4"
                 label="样本类型:"
                 label-for="type"
               >
-                <b-form-input list="type-list-id" size="sm"></b-form-input>
+                <b-form-input
+                  v-model="this.basicForm.samType"
+                  list="type-list-id"
+                  size="sm"
+                ></b-form-input>
                 <datalist id="type-list-id">
                   <option>Manual Option</option>
                   <option>Manual Option</option>
@@ -39,44 +104,173 @@
               </b-form-group>
 
               <b-form-group label-cols-sm="4" label="表型:" label-for="type">
-                <b-form-input list="form-list-id" size="sm"></b-form-input>
+                <b-form-input
+                  v-model="this.basicForm.reportType"
+                  list="form-list-id"
+                  size="sm"
+                ></b-form-input>
                 <datalist id="form-list-id">
                   <option>Manual Option</option>
                   <option>Manual Option</option>
                   <option>Manual Option</option>
                 </datalist>
               </b-form-group>
-            </b-card>
+            </div>
 
-            <b-card class="btnGroup">
+            <div class="basicInfo">
+              <h4>数据选取</h4>
+              <div style="margin-top: 30px">
+                <b-form-file
+                  v-model="file01"
+                  ref="file-input"
+                  class="mb-2"
+                ></b-form-file>
+              </div>
+              <h4 style="margin-bottom: 30px; margin-top: 30px">参考选取</h4>
+              <div style="margin-top: 30px">
+                <b-form-file
+                  v-model="file01"
+                  ref="file-input"
+                  class="mb-2"
+                ></b-form-file>
+              </div>
+
+              <div style="margin-top: 30px">
+                <b-button style="margin: 0 20px 0 0" variant="outline-success">
+                  保存
+                </b-button>
+
+                <b-button style="margin: 0 20px 0 0" variant="outline-danger">
+                  分析
+                </b-button>
+              </div>
+            </div>
+
+            <!-- <div class="accordion" role="tablist">
+    <h4 style="margin-bottom:30px">数据选取</h4>
+    <b-card no-body class="mb-1" style="width:500px">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-1 variant="outline-secondary">Accordion 1</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>I start opened because <code>visible</code> is <code>true</code></b-card-text>
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1" style="width:500px">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-2 variant="outline-secondary">Accordion 2</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1" style="width:500px">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-3 variant="outline-secondary">Accordion 3</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+  </div> -->
+
+            <!-- <div class="basicInfo">
+              <h4 style="margin-bottom:30px">参考选取</h4>
+              <b-card no-body class="mb-1">
+                <b-card-header header-tag="header" class="p-1" role="tab">
+                  <b-button block v-b-toggle.accordion-1 variant="info"
+                    >Accordion 1</b-button
+                  >
+                </b-card-header>
+                <b-collapse
+                  id="accordion-1"
+                  visible
+                  accordion="my-accordion"
+                  role="tabpanel"
+                >
+                  <b-card-body>
+                    <b-card-text
+                      >I start opened because <code>visible</code> is
+                      <code>true</code></b-card-text
+                    >
+                    <b-card-text>{{ text }}</b-card-text>
+                  </b-card-body>
+                </b-collapse>
+              </b-card>
+
+              <b-card no-body class="mb-1">
+                <b-card-header header-tag="header" class="p-1" role="tab">
+                  <b-button block v-b-toggle.accordion-2 variant="info"
+                    >Accordion 2</b-button
+                  >
+                </b-card-header>
+                <b-collapse
+                  id="accordion-2"
+                  accordion="my-accordion"
+                  role="tabpanel"
+                >
+                  <b-card-body>
+                    <b-card-text>{{ text }}</b-card-text>
+                  </b-card-body>
+                </b-collapse>
+              </b-card>
+
+              <b-card no-body class="mb-1">
+                <b-card-header header-tag="header" class="p-1" role="tab">
+                  <b-button block v-b-toggle.accordion-3 variant="info"
+                    >Accordion 3</b-button
+                  >
+                </b-card-header>
+                <b-collapse
+                  id="accordion-3"
+                  accordion="my-accordion"
+                  role="tabpanel"
+                >
+                  <b-card-body>
+                    <b-card-text>{{ text }}</b-card-text>
+                  </b-card-body>
+                </b-collapse>
+              </b-card>
+            </div> -->
+
+            <!-- <b-card class="btnGroup">
               <b-button variant="outline-primary">数据选取</b-button>
               <b-button variant="outline-primary">参考选取</b-button>
               <b-button variant="outline-primary">保存</b-button>
               <b-button variant="danger">分析</b-button>
-            </b-card>
+            </b-card> -->
 
-            <b-card class="analysisConfig" title="">
-              <b-table hover :items="inputs" :fields="inputs_fields">
-                <template v-slot:cell(数值)="">
-                  <b-form-input size="sm"></b-form-input>
-                </template>
-                <template v-slot:cell(操作)="row">
-                  <b-button
-                    @click="delConfig(row)"
-                    size="sm"
-                    variant="outline-danger"
-                    >删除</b-button
-                  >
-                </template>
-              </b-table>
-              <b-button
-                size="sm"
-                style="float: right; margin-right: 10px"
-                variant="outline-success"
-                @click="addConfig"
-                >增加</b-button
-              >
-            </b-card>
+            <!-- <b-card class="analysisConfig" title="">
+                <b-table hover :items="inputs" :fields="inputs_fields">
+                  <template v-slot:cell(数值)="">
+                    <b-form-input size="sm"></b-form-input>
+                  </template>
+                  <template v-slot:cell(操作)="row">
+                    <b-button
+                      @click="delConfig(row)"
+                      size="sm"
+                      variant="outline-danger"
+                      >删除</b-button
+                    >
+                  </template>
+                </b-table>
+                <b-button
+                  size="sm"
+                  style="float: right; margin-right: 10px"
+                  variant="outline-success"
+                  @click="addConfig"
+                  >增加</b-button
+                >
+              </b-card> -->
           </b-card-body>
         </b-tab>
 
@@ -111,7 +305,7 @@
 
 <script>
 import BreadCrumb from '@/components/BreadCrumb.vue'
-
+import XLSX from 'xlsx'
 export default {
   name: 'Desc01',
   components: { BreadCrumb },
@@ -120,6 +314,17 @@ export default {
       breadCrumb: [
         { href: '', name: '传感染mNGS临床检测分析', isActive: true }
       ],
+      basicForm: {
+        name: '',
+        age: '',
+        gender: '',
+        no: '',
+        samType: '',
+        reportType: ''
+      },
+      projectSelected: [],
+      projectList: [],
+      project_fields: ['选择', '客户/代理/销售名称', '客户编号', '诺因编号', '样本类型'],
       inputs_fields: ['参数名', '数值', '操作'],
       inputs: [
         { 参数名: 'MQ' },
@@ -142,6 +347,50 @@ export default {
     }
   },
   methods: {
+    clearFiles() {
+      this.$refs['file-proj'].reset()
+    },
+    projOnSelect() {
+      const proj = this.projectSelected[0]
+      this.basicForm = {
+        name: proj['客户/代理/销售名称'],
+        age: proj['年龄'],
+        gender: proj['性别'],
+        no: proj['诺因编号'],
+        samType: proj['样本类型'],
+        reportType: proj['报告版式']
+      }
+      console.log(this.basicForm)
+
+      this.$refs['proj-modal'].hide()
+    },
+    readExcel(e) {
+      const files = e.target.files
+      const fileReader = new FileReader()
+      fileReader.onload = ev => {
+        try {
+          const data = ev.target.result
+          const workbook = XLSX.read(data, { type: 'binary' })
+          const sheet = workbook.SheetNames[0] // 取第一张表 Sheet1
+          const ws = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]) // 生成json表格内容
+          this.projectList = []
+          ws.forEach((item) => { this.projectList.push(item) })
+          // console.log(this.projectList)
+          // this.projectList = ws
+          const selectButton = document.querySelector('#selectProj')
+          selectButton.click()
+        } catch (e) {
+          return false
+        }
+      }
+      fileReader.readAsBinaryString(files[0])
+    },
+    onRowSelected(items) {
+      this.projectSelected = items
+    },
+    parseBasicData(file) {
+      console.log(file)
+    },
     delConfig(row) {
       this.inputs.splice(row.index, 1)
     },
@@ -162,14 +411,21 @@ export default {
 .card {
   // border: 2px solid rgba(0, 0, 0, 0.125);
   .card-body {
+    .ways {
+      text-align: center;
+      button {
+        margin: 0 30px;
+      }
+    }
     .basicInfo {
       max-width: 50%;
       float: left;
       margin-left: 50px;
+      margin-right: 50px;
       margin-bottom: 40px;
     }
     .btnGroup {
-      max-width: 30%;
+      max-width: 40%;
       float: left;
       margin-left: 100px;
       border: 0px;
